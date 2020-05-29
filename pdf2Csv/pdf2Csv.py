c55d5bfd8b9f49ca069a2cf5dfca9de8aad309c0
prashant.shokeen <prashant.shokeen@eshopbox.com>
2020-05-29 19:44:19 +0530

    input file validation added output folder is created

--- pdf2Csv/pdf2Csv.py
+++ pdf2Csv/pdf2Csv.py
@@ -1,9 +1,11 @@
+import pandas
 import tabula
 import time
 import sys
+import os
+from os import path
 
-
-# from tabula import read_pdf
+outputPath = os.getcwd()+"/ouput/"
 
 
 
@@ -14,9 +16,10 @@ def convert(file):
     
     milliseconds = int(round(time.time() * 1000))
 
+    
     fileInputPath=file
-    fileTempPath="./"+"temp_"+str(milliseconds)+".csv"
-    fileOutputPath="BalSheet_"+str(milliseconds)+".csv"
+    fileTempPath=outputPath+"temp_"+str(milliseconds)+".csv"
+    fileOutputPath=outputPath+"BalSheet_"+str(milliseconds)+".csv"
 
 
     tabula.convert_into(fileInputPath, fileTempPath )
@@ -24,6 +27,31 @@ def convert(file):
 
     return fileOutputPath
 
+def validateFile(file):
+    ''' function to validate input file details 
+        [path,extension]
+    '''
+    if(path.exists(file) == False):
+        raise fileNotFound
+ 
+    if(file[-3:]!= "pdf"):
+        raise incorrectFileExtension
+
+def createFolder(path):
+    '''fucntion to create output path'''
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+class fileNotFound(Error):
+    """Raised when file not found"""
+    pass
+class incorrectFileExtension(Error):
+    """Raised when File with incorrect extension if provided in input"""
+    pass
 
 
 if __name__ == "__main__":
